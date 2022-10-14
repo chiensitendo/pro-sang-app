cd0e6c04aec8640caf9a483ad15fd5c743df6ed4
stran <stran@tripadvisor.com>
2022-10-14 15:37:42 +0700

    Google SEO 2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import {CSSProperties} from "react";
+import Script from "next/script";
 
 export default function Document() {
     const proLoadingCssStyles:  CSSProperties = {
@@ -25,8 +26,8 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"true"}/>
                 <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"/>
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia|Trirong|Audiowide|Tangerine|Roboto|Dancing+Script|Noto+Sans+JP|Noto+Music"/>
-                <script type="text/javascript" src="/analytics.js">
-                </script>
+                <Script type="text/javascript" src="/analytics.js">
+                </Script>
             </Head>
         <body>
             <div id="pro-loading" hidden style={proLoadingCssStyles}>
