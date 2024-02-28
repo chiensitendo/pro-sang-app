f625b1bd6413416b782e19a9fe4d63c9626d13d4
chiensitendo <sang9c.nguyendu@gmail.com>
2024-02-28 22:26:09 +0700

    [TEST] API

---
+++ app/test/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useEffect } from "react";
+
+
+const TestPage = () => {
+    useEffect(() => {
+        fetch("https://sangapi.pro.vn/api/ping", {
+            method: "GET"
+        }).then(res => console.log(res)).catch(t => console.log(t));
+    },[]);
+    return <div>Test</div>;
+}
+
+export default TestPage;
